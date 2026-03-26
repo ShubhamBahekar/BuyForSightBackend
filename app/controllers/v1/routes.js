15627d913242356bc1414003e791ed5377e04445
ShubhamBahekar <shubhambahekar02@gmail.com>
2026-03-26 08:01:32 +0530

    resolved shortend

--- app/controllers/v1/routes.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const users = require('./users/routes');
-
-// const router = express.Router();
-
-// router.use('/users', users);
-
-
-// module.exports = router;
